9cce0c578f52263b0e66f76b9f162b171423000a
Giorgio Ughini <giorgio1937@hotmail.it>
2020-03-24 10:43:47 +0100

    Fix typo

--- cmab_one_phase.py
+++ cmab_one_phase.py
@@ -11,9 +11,9 @@ total_budget = 75
 budgets_j = [np.arange(min_budgets[0], max_budgets[0] + 1, step), np.arange(min_budgets[1], max_budgets[1] + 1, step), np.arange(min_budgets[2], max_budgets[2] + 1, step)]      # +1 to max_budget because range does not include the right extreme of the interval by default
 n_arms = [len(budgets_j[0]), len(budgets_j[1]), len(budgets_j[2])]
 sigma = 10
-T = 200
+T = 400
 J = 3
-n_experiments = 75
+n_experiments = 3
 per_experiment_rewards_gpts = [[] for i in range(0, J)]
 
 #   This script, configured with n_experiments = 75, T = 200, and 6-7 arms for each userType
@@ -21,10 +21,11 @@ per_experiment_rewards_gpts = [[] for i in range(0, J)]
 
 for e in range(0, n_experiments):
     opt = CMABOptimizer(max_budget=total_budget, campaign_number=J, step=step)
-    envs = [CMABEnvironment(budgets=budgets_j[0], sigma=sigma, userType=0), WIPEnv(budgets=budgets_j[1], sigma=sigma, userType=1), WIPEnv(budgets=budgets_j[2], sigma=sigma, userType=2)]
+    envs = [CMABEnvironment(budgets=budgets_j[0], sigma=sigma, userType=0), CMABEnvironment(budgets=budgets_j[1], sigma=sigma, userType=1), CMABEnvironment(budgets=budgets_j[2], sigma=sigma, userType=2)]
     gpts_learners = [GPTS_Learner(n_arms=n_arms[0], arms=budgets_j[0]), GPTS_Learner(n_arms=n_arms[1], arms=budgets_j[1]), GPTS_Learner(n_arms=n_arms[2], arms=budgets_j[2])]
 
     for t in range(0, T):
+        # Create matrix for the optimization process by sampling the GPTS
         colNum = int(np.floor_divide(total_budget, step) + 1)
         base_matrix = np.ones((J, colNum)) * np.NINF
         for j in range(0, J):
@@ -33,8 +34,10 @@ for e in range(0, n_experiments):
             indices_list = [i for i in range(bubblesNum + colNum * j, bubblesNum + colNum * j + len(sampled_values))]
             np.put(base_matrix, indices_list, sampled_values)
 
+        # Choose budget thanks to the samples in the matrix
         chosen_budget = opt.optimize(base_matrix)
 
+        # Update model of the GPTS
         arms_chosen = []
         for j in range(0, J):
             chosen_arm = gpts_learners[j].convert_value_to_arm(chosen_budget[j])
@@ -42,6 +45,7 @@ for e in range(0, n_experiments):
             reward = envs[j].round(chosen_arm)
             gpts_learners[j].update(chosen_arm, reward)
 
+    # Append rewards for statistical purposes
     for j in range(0, J):
         per_experiment_rewards_gpts[j].append(gpts_learners[j].collected_rewards)
 
