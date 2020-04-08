827555fbd428090da75197847951559f45303306
FurlanAndrea <andrea.furlan20@gmail.com>
2020-04-08 13:30:14 +0200

    Clean main context generator

--- context_generator/main.py
+++ context_generator/main.py
@@ -1,44 +1,32 @@
-import seaborn as sb
-from tqdm import tqdm
-import matplotlib.pyplot as mplt
+import matplotlib.pyplot as plt
 import utils as u
 from context_generator.TSContextGenerator import *
 
-n_arms = 6
-arms = np.array([100 + 50 * i for i in range(n_arms)])
-class_probs = np.array([0.3, 0.5, 0.2])
-arm_dem_means = np.zeros([len(arms), len(class_probs)])
-for i in range(len(arms)):
-    for j in range(len(class_probs)):
-        arm_dem_means[i][j] = u.getDemandCurve(j, arms[i])
-clairs = []
-rews = []
-regrs = []
-T_HOR = 18250
-N_EXPS = 1
-for _ in tqdm(range(N_EXPS)):
-    contgen = TSContextGenerator(class_probs,
-                                     arm_dem_means,
-                                     arms,
-                                     ["A", "B", "C"])
-    for i in range(T_HOR):
-        contgen.calc_clair_rew_regret()
-        if i % 350 == 0:
-            contgen.split()
-    clairs.append(contgen.clairvoyants)
-    rews.append(contgen.rewards)
-    regrs.append(contgen.regrets)
-    contgen.print_tree()
-# sb.lineplot(range(T_HOR), u.smoothen_curve(np.mean(clairs, axis=0)))
-smooth_rews = u.smoothen_curve(np.mean(rews, axis=0))
-gr = sb.lineplot(range(T_HOR), smooth_rews)
-gr.set_title(str(n_arms) + " ARMS - REVENUE")
-mplt.show()
-smooth_regrs = u.smoothen_curve(np.mean(regrs, axis=0))
-gr = sb.lineplot(range(T_HOR), smooth_regrs)
-gr.set_title(str(n_arms) + " ARMS - REGRET")
-mplt.show()
-clairmean = np.mean(clairs, axis=0)
-gr = sb.lineplot(range(T_HOR), clairmean)
-gr.set_title(str(n_arms) + " ARMS - CLAIRVOYANT")
-mplt.show()
+regret = []
+number_of_arms = 6
+number_of_experiments = 1
+number_of_clicks = 18000
+interval_for_splitting = 300
+probabilities_of_users = [u.getProbabilities(0), u.getProbabilities(1), u.getProbabilities(2)]
+arms = np.array([100 + ((400-100)/number_of_arms) * i for i in range(number_of_arms)])
+arms_demand_for_each_user = np.zeros([number_of_arms, len(probabilities_of_users)])
+
+for i in range(number_of_arms):
+    for j in range(len(probabilities_of_users)):
+        arms_demand_for_each_user[i][j] = u.getDemandCurve(j, arms[i])
+
+for _ in range(number_of_experiments):
+    ts_context_generator = TSContextGenerator(probabilities_of_users, arms_demand_for_each_user, arms)
+    for i in range(number_of_clicks):
+        ts_context_generator.calc_clair_rew_regret()
+        if i % interval_for_splitting == 0:
+            ts_context_generator.split()
+
+    regret.append(ts_context_generator.regrets)
+
+plt.figure(0)
+plt.plot(np.cumsum(np.mean(regret, axis=0)))
+plt.legend(["6 arms - TS"])
+plt.xlabel("number of clicks")
+plt.ylabel("regret")
+plt.show()
