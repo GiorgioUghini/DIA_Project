edcf2d760f8ad67a46300f94d90315580e2ab97d
Giorgio Ughini <giorgio1937@hotmail.it>
2020-04-09 11:21:58 +0200

    Refactor code of Bdg+Pricing

--- BudgetAllocationAndPricing/main.py
+++ BudgetAllocationAndPricing/main.py
@@ -7,33 +7,40 @@ import math
 from scipy import optimize
 
 
-T = 364
+TIME_SPAN = 364
 N_CLASSES = 3
-n_experiments = 50
+N_EXPERIMENTS = 50
 
-step = 2
 min_budgets = [10, 10, 10]
 max_budgets = [54, 58, 52]
-sigma = 100
 total_budget = 90
+step = 2
+sigma = 100
+
 # budgets_j = [ [0, 2, 4, ..., 34] , [0, 2, 4, ..., 38], [0, 2, 4, ..., 36] ]
-budgets_j = [np.arange(min_budgets[0], max_budgets[0] + 1, step), np.arange(min_budgets[1], max_budgets[1] + 1, step), np.arange(min_budgets[2], max_budgets[2] + 1, step)]      # +1 to max_budget because range does not include the right extreme of the interval by default
-bdg_n_arms = [len(budgets_j[0]), len(budgets_j[1]), len(budgets_j[2])]
+# +1 to max_budget because range does not include the right extreme of the interval by default
+budgets_j = [np.arange(min_budgets[x], max_budgets[x] + 1, step) for x in range(0, N_CLASSES)]
+bdg_n_arms = [len(budgets_j[x]) for x in range(0, N_CLASSES)]
 per_experiment_revenues = []
 
-pr_n_arms = math.ceil((T * np.log10(T)) ** 0.25)  # the optimal number of arms
-best_prices = [ optimize.fmin(lambda x: -utils.getDemandCurve(j, x) * x, T / 2)[0] for j in range(0,N_CLASSES) ]
-best_demand = [ utils.getDemandCurve(j, best_prices[j]) * best_prices[j] for j in range(0,N_CLASSES) ]
+pr_n_arms = math.ceil((TIME_SPAN * np.log10(TIME_SPAN)) ** 0.25)  # the optimal number of arms
+best_prices = [optimize.fmin(lambda x: -utils.getDemandCurve(j, x) * x, TIME_SPAN / 2)[0]
+               for j in range(0, N_CLASSES)]
+best_values_per_click = [utils.getDemandCurve(j, best_prices[j]) * best_prices[j]
+                         for j in range(0, N_CLASSES)]
 
 
-for e in range(0, n_experiments):
+for e in range(0, N_EXPERIMENTS):
     opt = CMABOptimizer(max_budget=total_budget, campaign_number=N_CLASSES, step=step)
-    env = MainEnvironment(budgets_list=budgets_j, sigma=sigma, pr_n_arms=[pr_n_arms, pr_n_arms, pr_n_arms], pr_minPrice=[100,100,100], pr_maxPrice=[400,400,400])
-    gpts_learners = [ GPTS_Learner(n_arms=bdg_n_arms[v], arms=budgets_j[v]) for v in range(0, N_CLASSES) ]
+    env = MainEnvironment(budgets_list=budgets_j, sigma=sigma,
+                          pr_n_arms=[pr_n_arms for _ in range(0, N_CLASSES)],
+                          pr_minPrice=[100, 100, 100], pr_maxPrice=[400, 400, 400])
+    gpts_learners = [ GPTS_Learner(n_arms=bdg_n_arms[v], arms=budgets_j[v])
+                      for v in range(0, N_CLASSES) ]
     pr_ts_learners = [ TS_Learner(arms=env.pr_probabilities[v]) for v in range(0, N_CLASSES) ]
     experiment_revenues = []
 
-    for t in range(0, T):
+    for t in range(0, TIME_SPAN):
         # Create matrix for the optimization process by sampling the GPTS
         colNum = int(np.floor_divide(total_budget, step) + 1)
         base_matrix = np.ones((N_CLASSES, colNum)) * np.NINF
@@ -46,25 +53,26 @@ for e in range(0, n_experiments):
         # Choose budget thanks to the samples in the matrix
         chosen_budget = opt.optimize(base_matrix)
 
-        # Update model of the GPTS
-        arms_chosen = []
-        revenue = 0
+        aggregated_revenue = 0
         for j in range(0, N_CLASSES):
+            # Update model of the GPTS
             chosen_arm = gpts_learners[j].convert_value_to_arm(chosen_budget[j])
             chosen_arm = int(chosen_arm[0])
             clicks = env.round_budget(chosen_arm, j)
             gpts_learners[j].update(chosen_arm, clicks)
 
-            # Pricing
+            # Pricing: problem can be decomposed. The optimal solution is the union
+            # of the three optimal sub-solution as the seller can set a different price
             pulled_arm = pr_ts_learners[j].pull_arm()
-            successes = env.round_pricing(pulled_arm, clicks, j)
+            successes = env.round_pricing(pulled_arm, clicks, j)    # Successful clicks with current budget
             failures = clicks - successes
             pr_ts_learners[j].update(pulled_arm, successes, failures)
-            revenue += successes * env.pr_probabilities[j][pulled_arm][0]
+            aggregated_revenue += successes * env.pr_probabilities[j][pulled_arm][0]    # For all classes
 
-        experiment_revenues.append(revenue)
+        # Append the revenue of this day to the array for this experiment
+        experiment_revenues.append(aggregated_revenue)
 
-    # Append rewards for statistical purposes
+    # Append the array of revenues of this experiment to the main one, for statistical purposes
     per_experiment_revenues.append(experiment_revenues)
 
 
@@ -79,17 +87,27 @@ for j in range(0, N_CLASSES):
 chosen_budget = opt.optimize(base_matrix)
 chosen_arms = [gpts_learners[j].convert_value_to_arm(chosen_budget[j])[0] for j in range(0, N_CLASSES)]
 optimized_alloc = [env.means[j][chosen_arms[j]] for j in range(0, N_CLASSES)]
-optimized_revenue = [optimized_alloc[j] * best_demand[j] for j in range(0, N_CLASSES)]
+optimized_revenue = [optimized_alloc[j] * best_values_per_click[j] for j in range(0, N_CLASSES)]
 
 # Print aggregated results
 aggr_optimal_revenue = np.sum(optimized_revenue)
 aggr_rewards = np.mean(per_experiment_revenues, axis=0)
 plt.figure(0)
 plt.ylabel("Regret")
-plt.xlabel("t")
+plt.xlabel("time")
 a = aggr_optimal_revenue - aggr_rewards
 c = np.cumsum(a)
 plt.plot(c, 'r')
-plt.legend(["GPTS Learner"])
+plt.legend(["Budget + Pricing"])
+plt.show()
+
+plt.figure(1)
+plt.ylabel("Revenue")
+plt.xlabel("time")
+a = aggr_rewards
+b = np.cumsum(aggr_optimal_revenue)
+plt.plot(a, 'b')
+plt.plot(b, 'k--')
+plt.legend(["Algorithm", "Optimal"])
 plt.show()
 
