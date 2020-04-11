7c9ec1f1bdbcfa09657e45ada11f69b768e7c956
Giorgio Ughini <giorgio1937@hotmail.it>
2020-04-11 20:13:29 +0200

    Update parameters to perform computations

--- context_generator/main.py
+++ context_generator/main.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import utils as u
 from context_generator.TSContextGenerator import *
+from datetime import datetime
 
 regret = []
 number_of_arms = 6
-number_of_experiments = 10
-number_of_clicks = 14000
-interval_for_splitting = 700
+number_of_experiments = 5
+number_of_clicks = 280000   # 20 days
+interval_for_splitting = 14000   # 2000 clicks per day
 probabilities_of_users = [u.getProbabilities(0), u.getProbabilities(1), u.getProbabilities(2)]
 arms = np.array([100 + ((400-100)/number_of_arms) * i for i in range(number_of_arms)])
 arms_demand_for_each_user = np.zeros([number_of_arms, len(probabilities_of_users)])
@@ -20,7 +21,8 @@ for e in range(number_of_experiments):
     for i in range(number_of_clicks):
         ts_context_generator.calc_clair_rew_regret()
         if i % interval_for_splitting == 0:
-            print("Step %s of %s (%s exp)" % ((i/interval_for_splitting)+1, number_of_clicks/interval_for_splitting, e+1))
+            timestampStr = datetime.now().strftime("%H:%M:%S")
+            print(timestampStr + " - Step %s of %s (%s exp)" % ((i/interval_for_splitting)+1, number_of_clicks/interval_for_splitting, e+1))
             ts_context_generator.split()
 
     regret.append(ts_context_generator.regrets)
