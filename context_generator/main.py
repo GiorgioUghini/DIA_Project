5770bbe7db229f6afc2e06b3f86cf0b10230df5a
FurlanAndrea <andrea.furlan20@gmail.com>
2020-04-11 15:14:38 +0200

    Quick fix

--- context_generator/main.py
+++ context_generator/main.py
@@ -20,7 +20,7 @@ for e in range(number_of_experiments):
     for i in range(number_of_clicks):
         ts_context_generator.calc_clair_rew_regret()
         if i % interval_for_splitting == 0:
-            print("Step %s of %s (%s exp)" % (i/interval_for_splitting, number_of_clicks/interval_for_splitting, e))
+            print("Step %s of %s (%s exp)" % ((i/interval_for_splitting)+1, number_of_clicks/interval_for_splitting, e+1))
             ts_context_generator.split()
 
     regret.append(ts_context_generator.regrets)
