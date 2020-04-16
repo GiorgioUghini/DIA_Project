150025b36c7ffcc93d4593dbcffeefebbe475a72
Giorgio Ughini <giorgio1937@hotmail.it>
2020-04-16 17:44:01 +0200

    Change prob/clicks function of all phases

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+def hs(x):
+    return np.piecewise(x, [x < 0, x >= 0], [0, 1])
 
 def getClickCurve(phase, userType, x_values):
     if(phase == 0):
         # High interest / No competitors
         if(userType == 0):
-            return 12700 * np.log(x_values/ 27 + 1)
+            return 10900 * np.log(x_values/ 19 + 1)
         elif(userType == 1):
             return 14500 * np.log(x_values/ 20 + 1)
         else:
@@ -13,27 +15,27 @@ def getClickCurve(phase, userType, x_values):
     elif(phase == 1):
         # Low interest / No competitors
         if(userType == 0):
-            return 3150 * np.log(x_values / 7.5 + 1)
+            return 2450 * np.log(x_values / 3.5 + 1) - 15*(x_values - 17)*hs(x_values - 17)
         elif(userType == 1):
-            return 3900 * np.log(x_values / 5 + 1)
+            return 3900 * np.log(x_values / 5 + 1) - 25*(x_values - 17)*hs(x_values - 17)
         else:
-            return 2300 * np.log(x_values / 7.5 + 1)
+            return 2300 * np.log(x_values / 7.5 + 1) - 13*(x_values - 17)*hs(x_values - 17)
     elif (phase == 2):
         # Low interest / With competitors
         if(userType == 0):
-            return -13500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(70, 2.))) + 13500
+            return -7000 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(38, 2.))) + 7000
         elif(userType == 1):
-            return -14000 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(50, 2.))) + 14000
+            return -9500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(40, 2.))) + 9500
         else:
-            return -11500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(90, 2.))) + 11500
+            return -4500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(35, 2.))) + 4500
     else:
         # High interest / With competitors
         if (userType == 0):
-            return -21500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(55, 2.))) + 21500
+            return -23500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(55, 2.))) + 23500
         elif (userType == 1):
-            return -35000 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(65, 2.))) + 35000
+            return -37000 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(65, 2.))) + 37000
         else:
-            return -11500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(35, 2.))) + 11500
+            return -12500 * np.exp(-np.power(x_values- 0, 2.) / (2 * np.power(42, 2.))) + 12500
 
 
 def getProbabilities(userType):
