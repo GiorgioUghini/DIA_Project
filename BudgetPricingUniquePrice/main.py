e691503495af897d345c8d7369a8cb7403bacbff
Cosimo Russo <cosimo@menuale.com>
2020-06-14 22:11:59 +0200

    Prints results which actually make sense

--- BudgetPricingUniquePrice/main.py
+++ BudgetPricingUniquePrice/main.py
@@ -8,9 +8,9 @@ import csv
 from datetime import datetime
 
 
-TIME_SPAN = 2
+TIME_SPAN = 50
 N_CLASSES = 3
-N_EXPERIMENTS = 1
+N_EXPERIMENTS = 10
 
 min_budgets = [10, 10, 10]
 max_budgets = [54, 58, 52]
@@ -44,9 +44,10 @@ for e in range(0, N_EXPERIMENTS):
 
     for t in range(0, TIME_SPAN):
         # pull a price from each TS learner
-        sampled_prices = [pr_ts_learners[j].pull_arm() for j in range(0, N_CLASSES)]
+        sampled_prices_arms = [pr_ts_learners[j].pull_arm() for j in range(N_CLASSES)]
+        sampled_prices = [pr_ts_learners[j].arms[sampled_prices_arms[j]][0] for j in range(N_CLASSES)]
         # and get the corresponding conversion rate
-        conversion_rates = [pr_ts_learners[j].get_conversion_rate(sampled_prices[j]) for j in range(0, N_CLASSES)]
+        conversion_rates = [pr_ts_learners[j].get_conversion_rate(sampled_prices_arms[j]) for j in range(0, N_CLASSES)]
         # here we will store the expected revenue for each price
         expected_revenues = np.zeros(3)
         chosen_budgets = []
@@ -66,16 +67,15 @@ for e in range(0, N_EXPERIMENTS):
             # now we can solve the knapsack problem to find best allocation and expected revenue
             second_stage_rows = []
             for i in range(0, N_CLASSES):
-                matrix = np.atleast_2d(values_per_click[i]).T * clicks[i]  # valPerClick * clicks(budget)
-                second_stage_rows.append(np.amax(matrix, axis=1))  # remove dependency of price: sum along rows
+                second_stage_rows.append(values_per_click[i] * clicks[i])
 
             colNum = int(np.floor_divide(total_budget, step) + 1)  # that is, int(np.floor_divide(total_budget, step) + 1)
             base_matrix = np.ones((N_CLASSES, colNum)) * np.NINF
-            for j in range(0, N_CLASSES):
-                sampled_values = second_stage_rows[j]  # Row obtained in the first step maximizing elements
-                bubblesNum = int(min_budgets[j] / step)
-                indices_list = [i for i in
-                                range(bubblesNum + colNum * j, bubblesNum + colNum * j + len(sampled_values))]
+            for i in range(0, N_CLASSES):
+                sampled_values = second_stage_rows[i]  # Row obtained in the first step maximizing elements
+                bubblesNum = int(min_budgets[i] / step)
+                indices_list = [k for k in
+                                range(bubblesNum + colNum * i, bubblesNum + colNum * i + len(sampled_values))]
                 np.put(base_matrix, indices_list, sampled_values)
 
             # Choose budget thanks to the samples in the matrix
@@ -85,7 +85,7 @@ for e in range(0, N_EXPERIMENTS):
 
         # after the estimation is done with all prices, pick the price that corresponds to the maximum expected value
         best_TS_learner = int(np.argmax(expected_revenues))
-        best_price_arm = sampled_prices[best_TS_learner]
+        best_price_arm = sampled_prices_arms[best_TS_learner]
         best_price = pr_ts_learners[best_TS_learner].arms[best_price_arm][0]
         # and pick the arms that correspond to the best budget chosen
         best_budget_arms = [gpts_learners[i].convert_value_to_arm(chosen_budgets[best_TS_learner][i])[0][0] for i in range(N_CLASSES)]
@@ -113,30 +113,32 @@ for e in range(0, N_EXPERIMENTS):
     per_experiment_revenues.append(experiment_revenues)
 
 
-# Compute the REAL optimum allocation by solving the optimization problem with the real values
-second_stage_rows = []
-for userType in range(0, N_CLASSES):
-    clicks = env.means[userType]
-    prices = env.pr_probabilities[userType]
-    rows = len(prices)
-    cols = len(clicks)
-    values_per_click = np.zeros((rows, cols))
-    matrix = np.zeros((rows, cols))
-    for i in range(rows):
-        for j in range(cols):
-            matrix[i, j] = prices[i, 0] * clicks[j] * prices[i, 1] - budgets_j[userType][j]
-            values_per_click[i, j] = matrix[i, j] / clicks[j]
-    second_stage_rows.append(np.amax(matrix, axis=1))
+# Compute the REAL optimum allocation by solving the optimization problem with the real values for all prices and then get the best reward
+rewards_per_price = []
+prices = env.pr_probabilities[0][:, 0]
+for price in prices:
+    second_stage_rows = []
+    for userType in range(0, N_CLASSES):
+        clicks = env.means[userType]
+        demandCurve = demand(userType, price)
+        values_per_click = (price * clicks * demandCurve - budgets_j[userType]) / clicks
+        second_stage_rows.append(values_per_click * clicks)
 
-colNum = int(np.floor_divide(total_budget, step) + 1)  # that is, int(np.floor_divide(total_budget, step) + 1)
-base_matrix = np.ones((N_CLASSES, colNum)) * np.NINF
-for j in range(0, N_CLASSES):
-    sampled_values = second_stage_rows[j]  # Row obtained in the first step maximizing elements
-    bubblesNum = int(min_budgets[j] / step)
-    indices_list = [i for i in range(bubblesNum + colNum * j, bubblesNum + colNum * j + len(sampled_values))]
-    np.put(base_matrix, indices_list, sampled_values)
-chosen_budget = opt.optimize(base_matrix)
-optimized_revenue = opt.best_revenue
+    colNum = int(np.floor_divide(total_budget, step) + 1)  # that is, int(np.floor_divide(total_budget, step) + 1)
+    base_matrix = np.ones((N_CLASSES, colNum)) * np.NINF
+    for j in range(0, N_CLASSES):
+        sampled_values = second_stage_rows[j]  # Row obtained in the first step maximizing elements
+        bubblesNum = int(min_budgets[j] / step)
+        indices_list = [i for i in range(bubblesNum + colNum * j, bubblesNum + colNum * j + len(sampled_values))]
+        np.put(base_matrix, indices_list, sampled_values)
+    chosen_budget = opt.optimize(base_matrix)
+    rewards_per_price.append(opt.best_revenue)
+
+optimized_revenue_index = np.argmax(rewards_per_price)
+optimized_revenue = rewards_per_price[optimized_revenue_index]
+best_price = prices[optimized_revenue_index]
+print("Best price: " + str(best_price))
+print("Optimized revenue: " + str(optimized_revenue))
 
 # Storing results
 timestamp = str(datetime.timestamp(datetime.now()))
