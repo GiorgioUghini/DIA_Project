26d36835a66389218ac80aa66128ac3c45eac6cf
Cosimo Russo <cosimo@menuale.com>
2020-06-23 13:25:56 +0200

    Update pricing

--- PricingForClicks/PricingForClicks3/main.py
+++ PricingForClicks/PricingForClicks3/main.py
@@ -20,8 +20,10 @@ best_n_arms = 4  # math.ceil((T * np.log10(T)) ** 0.25)  # the optimal number of
 
 print("Optimal number of arms: %d" % best_n_arms)
 
-n_experiments = 100
-n_arms_arr = range(4, 10) if multi_arms else [best_n_arms]
+n_experiments = 1000
+MIN_N_ARMS = 3
+MAX_N_ARMS = 15
+n_arms_arr = list(range(MIN_N_ARMS, MAX_N_ARMS)) if multi_arms else [best_n_arms]
 
 max_regret_per_arm = []
 
@@ -30,6 +32,7 @@ real_best_prices = [np.argmax(demand[c](x) * x) for c in CLASSES]
 real_optimum = [demand[c](real_best_prices[c]) * real_best_prices[c] * clicks[c] for c in CLASSES]
 
 ts_regret_per_arm = []
+ts_reward_per_arm = []
 for n_arms in n_arms_arr:
     print(n_arms, "arms")
     ts_env = [Environment(n_arms=n_arms, demandCurve=demand[c], minPrice=0, maxPrice=400) for c in CLASSES]
@@ -40,34 +43,44 @@ for n_arms in n_arms_arr:
     print("Theoretical best prices for %d arms: %s" % (n_arms, str(best_prices)))
     optimum = [demand[j](best_prices[j]) * best_prices[j] * clicks[j] for j in CLASSES]
 
-    all_ts_regret = []
+    per_experiment_regret = []
+    per_experiment_reward = []
     for e in range(0, n_experiments):
         ts_learners = [TS_Learner(arms=t.probabilities) for t in ts_env]
-        ts_regret = []
+        daily_regret = []
+        daily_reward = []
         for t in range(0, T):
             ts_regret_per_class = []
+            ts_reward_per_class = []
             for userType in CLASSES:
                 # Thompson Sampling Learner
                 pulled_arm = ts_learners[userType].pull_arm()
+                pulled_price = ts_env[userType].probabilities[pulled_arm][0]
                 successes = ts_env[userType].round(pulled_arm, clicks[userType][t])
                 failures = clicks[userType][t] - successes
                 ts_learners[userType].update(pulled_arm, successes, failures)
-                actual_value = successes*ts_env[userType].probabilities[pulled_arm][0]
-                regret = real_optimum[userType][t] - actual_value
+                reward = successes * pulled_price
+                regret = optimum[userType][t] - reward
+                # append normalized reward
+                ts_reward_per_class.append(reward) # / clicks[userType][t])
                 ts_regret_per_class.append(regret)
 
-            ts_regret.append(ts_regret_per_class)
+            daily_regret.append(ts_regret_per_class)
+            daily_reward.append(ts_reward_per_class)
 
-        all_ts_regret.append(ts_regret)
+        per_experiment_regret.append(daily_regret)
+        per_experiment_reward.append(daily_reward)
 
-    ts_regret = np.cumsum(np.mean(np.sum(all_ts_regret, axis=2), axis=0))
-    max_regret_per_arm.append(ts_regret[len(ts_regret)-1])
-    ts_regret_per_arm.append(ts_regret)
+    tot_regret = np.cumsum(np.mean(np.sum(per_experiment_regret, axis=2), axis=0))
+    tot_reward = np.mean(np.sum(per_experiment_reward, axis=2), axis=0)
+    max_regret_per_arm.append(tot_regret[len(tot_regret) - 1])
+    ts_regret_per_arm.append(tot_regret)
+    ts_reward_per_arm.append(tot_reward)
 
-tmp = np.argsort(max_regret_per_arm) + 4
+tmp = np.argsort(max_regret_per_arm) + MIN_N_ARMS
 print("regrets per arm: %s" % str(np.sort(max_regret_per_arm)))
 print("Sorted arms: %s" % str(tmp))
-print("With %d arms we have the smallest regret: %d" % (4 + np.argmin(max_regret_per_arm), int(np.min(max_regret_per_arm))))
+print("With %d arms we have the smallest regret: %d" % (3 + np.argmin(max_regret_per_arm), int(np.min(max_regret_per_arm))))
 
 plt.figure(0)
 for n in range(len(n_arms_arr)):
@@ -76,3 +89,28 @@ plt.legend(n_arms_arr)
 plt.xlabel("T")
 plt.ylabel("Regret[€], TS")
 plt.show()
+
+plt.figure(1)
+plt.plot(np.sum(real_optimum, axis=0), '--')
+for n in range(len(n_arms_arr)):
+    plt.plot(ts_reward_per_arm[n])
+legend = ["clairvoyant"] + n_arms_arr
+plt.legend(legend)
+plt.xlabel("T")
+plt.ylabel("Reward[€], TS")
+plt.show()
+
+plt.figure(2)
+plt.plot(np.cumsum(np.sum(real_optimum, axis=0)), '--')
+cumulative_rewards = []
+for n in range(len(n_arms_arr)):
+    c = np.cumsum(ts_reward_per_arm[n])
+    cumulative_rewards.append(c[len(c)-1])
+    plt.plot(c)
+plt.legend(legend)
+plt.xlabel("T")
+plt.ylabel("Cumulative Reward[€], TS")
+plt.show()
+
+print("Cumulative rewards: %s" % str(cumulative_rewards))
+print("Arms sorted by reward: %s" % str(np.flip(np.argsort(cumulative_rewards)) + MIN_N_ARMS))
